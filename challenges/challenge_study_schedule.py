92a0505669d3e1607794fbdc608ef05b0476b33e
Leandro Batista <leandrobcrv@gmail.com>
2023-07-13 14:43:39 -0300

    Requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    try:
+        for inicio, fim in permanence_period:
+            if inicio <= int(target_time) <= fim:
+                counter += 1
+    except TypeError:
+        return None
+
+    return counter
